f601e35c1f57fb8878f84ee2d6a2417374ac43ae
Jieun Kim <jieunkim1379@gmail.com>
2024-08-28 01:55:24 -0400

    added form on contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,6 +11,34 @@ const Contact = () => {
         <h3>(540) XXX-XXXX</h3>
         <h2>Email</h2>
         <h3>hello@drycleaners.com</h3>
+        <div>
+          <form>
+            <label>
+              First Name
+              <input name='first_name' />
+            </label>
+
+            <label>
+              Last Name
+              <input name='last_name' />
+            </label>
+
+            <label>
+              Phone Number
+              <input type='tel' name='phone_number' />
+            </label>
+
+            <label>
+                Email 
+                <input type='email' name='email'/>
+            </label>
+
+            <label>
+                Message 
+                <input type='text' name='message'/>
+            </label>
+          </form>
+        </div>
         {/* Customer Inquiry Form */}
       </div>
     </div>
