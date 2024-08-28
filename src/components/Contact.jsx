5d1670d54374adf4a62d60b191d162b20db8157c
Jieun Kim <jieunkim1379@gmail.com>
2024-08-28 16:29:37 -0400

    worked on contact tab css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,42 +6,57 @@ const Contact = () => {
       <div className='pageTop'>
         <h1>Contact Info</h1>
       </div>
-      <div>
-        <h2>Phone Number</h2>
-        <h3>(540) XXX-XXXX</h3>
-        <h2>Email</h2>
-        <h3>hello@drycleaners.com</h3>
-        <div>
-          <form>
-            <label>
-              First Name
-              <input name='first_name' />
-            </label>
+      <div className='pageBottom'>
+        <div id='contact-info'>
+          <h2>Phone Number</h2>
+          <h3>(540) XXX-XXXX</h3>
+          <h2>Email</h2>
+          <h3>hello@drycleaners.com</h3>
+        </div>
 
-            <label>
-              Last Name
-              <input name='last_name' />
-            </label>
+        <div id='question-form-container'>
+          <h2 id='sentence'>Have any questions or comments? Feel free to contact us anytime!</h2>
+          <form id='question-form'>
+            <div id='question-form-name'>
+              <label className='form-label'>
+                First Name
+                <input name='first_name' className='question-input' />
+              </label>
 
-            <label>
-              Phone Number
-              <input type='tel' name='phone_number' />
-            </label>
+              <label className='form-label'>
+                Last Name
+                <input name='last_name' className='question-input' />
+              </label>
+            </div>
 
-            <label>
-                Email 
-                <input type='email' name='email'/>
-            </label>
+            <div id='question-form-info'>
+              <label className='form-label form-label-info'>
+                Phone Number
+                <input
+                  type='tel'
+                  name='phone'
+                  className='question-input'
+                />
+              </label>
 
-            <label>
-                Message 
-                <input type='text' name='message'/>
-            </label>
+              <label className='form-label form-label-info'>
+                Email Address
+                <input type='email' name='email' className='question-input' />
+              </label>
+            </div>
 
-            <button>Submit</button>
+            <div id='question-form-message'>
+              <label className='form-label'>Message</label>
+              <textarea
+                name='message'
+                className='question-input'
+                placeholder='Questions or comments?'
+              />
+            </div>
+
+            <button>Send</button>
           </form>
         </div>
-        {/* Customer Inquiry Form */}
       </div>
     </div>
   );
