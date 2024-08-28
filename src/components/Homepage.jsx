703b520e92dd5c9d3f82f08ce0dfb06ee1b32b5a
Jieun Kim <jieunkim1379@gmail.com>
2024-08-28 00:26:06 -0400

    added hours on homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,7 +3,22 @@ import React from "react";
 function Homepage() {
     return (
         <div id='homepage'>
-            <h1>Hello</h1>
+            <h1>Welcome to Dry Cleaners</h1>
+            <section id='operatingHours'>
+                <h2>Hours of Operation</h2>
+                <div className="timeSection">
+                    <h3>Monday-Friday:</h3>
+                    <h3>8:00am - 6:00pm</h3>
+                </div>
+                <div className="timeSection">
+                    <h3>Saturday:</h3>
+                    <h3>9:00am - 1:00pm</h3>
+                </div>
+                <div className="timeSection">
+                    <h3>Sunday:</h3>
+                    <h3 id='closed'>Closed</h3>
+                </div>
+            </section>
         </div>
     )
 }
