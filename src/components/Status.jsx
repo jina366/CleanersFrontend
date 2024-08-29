b4a2e042d7b607bdcaada31668ddf13a78575f32
Jieun Kim <jieunkim1379@gmail.com>
2024-08-28 20:38:32 -0400

    made status search form

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -6,11 +6,12 @@ const Status = () => {
       <div className='pageTop'>
         <h1>Status Check</h1>
       </div>
-      <div>
-        <form>
-            <label>
+      <div id='status-container'>
+        <h2 id='status-sentence'>Want to check the status of your items? Search using your ticket number.</h2>
+        <form id='status-form'>
+            <label className='form-label'>
             Ticket Number 
-            <input type='number'/>
+            <input type='number' className='status-input' placeholder='XXX-XXX'/>
             </label>
 
             <button>Submit</button>
